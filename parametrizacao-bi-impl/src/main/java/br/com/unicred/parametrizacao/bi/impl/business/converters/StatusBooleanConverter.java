6ac7d7174e8e1a0c8564683c6491f6835122f40b
Gabriel Correia Gonçalves <gabrielc904@gmail.com>
2018-12-07 15:44:42 -0200

    criacao de status converter

---
+++ parametrizacao-bi-impl/src/main/java/br/com/unicred/parametrizacao/bi/impl/business/converters/StatusBooleanConverter.java
@@ -0,0 +1,24 @@
+package br.com.unicred.parametrizacao.bi.impl.business.converters;
+
+import javax.persistence.AttributeConverter;
+
+public class StatusBooleanConverter implements AttributeConverter<Boolean, String> {
+	
+	@Override
+	public String convertToDatabaseColumn(Boolean attribute) {
+		if (attribute) {
+			return "S";
+		} else {
+			return "N";
+		}
+	}
+	
+	@Override
+	public Boolean convertToEntityAttribute(String dbData) {
+		if (dbData == "S") {
+			return true;
+		} else {
+			return false;
+		}
+	}
+}
