29f012799f9b9aae83c43faa1b16019f560a89fd
francieli.bedin <francieli.bedin@UBR-RS-1449.e-unicred.com.br>
2018-12-07 17:57:35 -0200

    Mensagens validações na inclusão do indicador.

--- parametrizacao-bi-impl/src/main/java/br/com/unicred/parametrizacao/bi/impl/business/commands/IndicadorEdicaoCommand.java
+++ parametrizacao-bi-impl/src/main/java/br/com/unicred/parametrizacao/bi/impl/business/commands/IndicadorEdicaoCommand.java
@@ -1,43 +1,22 @@
 package br.com.unicred.parametrizacao.bi.impl.business.commands;
 
-
-import javax.validation.constraints.NotNull;
-import javax.validation.constraints.Size;
-
 import br.com.unicred.parametrizacao.bi.impl.business.validators.CommandValidator;
 
-public class IndicadorInclusaoCommand {
+public class IndicadorEdicaoCommand {
 	
-	@NotNull
-	private Integer codigo;
-	
-//	@NotNull(message = "{indicador.nome.nao.nulo}")
-
-	@Size(min = 1, max = 100)
 	private String nome;
-	
-	@NotNull
-	@Size(min = 1, max = 1)
 	private String periodicidade;
 	
 	
-	public IndicadorInclusaoCommand() {
+	public IndicadorEdicaoCommand() {
 		
 	}
 	
 	public void validate() {
-		CommandValidator<IndicadorInclusaoCommand> validator = 
-				new CommandValidator<IndicadorInclusaoCommand>();
+		CommandValidator<IndicadorEdicaoCommand> validator = 
+				new CommandValidator<IndicadorEdicaoCommand>();
 		
 		validator.validate(this);
-	}
-
-	public Integer getCodigo() {
-		return codigo;
-	}
-
-	public void setCodigo(Integer codigo) {
-		this.codigo = codigo;
 	}
 
 	public String getNome() {
