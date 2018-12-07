d43a6f3672dae09cfde10b205feaf5c855c5c6c4
Gabriel Correia Gonçalves <gabrielc904@gmail.com>
2018-12-07 16:34:23 -0200

    criacao command periodo fechamento ppr

---
+++ parametrizacao-bi-impl/src/main/java/br/com/unicred/parametrizacao/bi/impl/business/commands/PeriodoFechamentoPPRCommand.java
@@ -0,0 +1,42 @@
+package br.com.unicred.parametrizacao.bi.impl.business.commands;
+
+import java.time.LocalDate;
+
+public class PeriodoFechamentoPPRCommand {
+	
+	private LocalDate dataCompetencia;
+    private LocalDate dataUltimoProcessamento;
+    private boolean isFechado;
+    
+    public PeriodoFechamentoPPRCommand(){
+        
+    }
+    
+    public void validate() {
+    	//TODO realizar validacao
+    }
+    
+    public LocalDate getDataCompetencia() {
+        return dataCompetencia;
+    }
+
+    public void setDataCompetencia(LocalDate dataCompetencia) {
+        this.dataCompetencia = dataCompetencia;
+    }
+
+    public LocalDate getDataUltimoProcessamento() {
+        return dataUltimoProcessamento;
+    }
+
+    public void setDataUltimoProcessamento(LocalDate dataUltimoProcessamento) {
+        this.dataUltimoProcessamento = dataUltimoProcessamento;
+    }
+
+    public boolean isFechado() {
+        return isFechado;
+    }
+
+    public void setFechado(boolean isFechado) {
+        this.isFechado = isFechado;
+    }
+}
