a7512e9ef2e82cad66d58ba8e29c9a02db2a40c8
Unicred do Brasil - Vanderlei Brondani Davila <vanderlei.davila@e-unicred.com.br>
2018-12-07 11:47:29 -0200

    criar_domain_command_DefinicaoContasContabeisOrcadoRealizadoProjetosCommand)

---
+++ parametrizacao-bi-api/src/main/java/br/com/unicred/parametrizacao/bi/api/v1/commands/DefinicaoContasContabeisOrcadoRealizadoProjetosCommand.java
@@ -0,0 +1,36 @@
+package br.com.unicred.parametrizacao.bi.api.v1.commands;
+
+public class DefinicaoContasContabeisOrcadoRealizadoProjetosCommand {
+
+    private Integer codigoCooperativa;
+    private String codigoContaEstrutural;
+    private String comparacao;
+
+    public Integer getCodigoCooperativa() {
+        return codigoCooperativa;
+    }
+
+    public void setCodigoCooperativa(Integer codigoCooperativa) {
+        this.codigoCooperativa = codigoCooperativa;
+    }
+
+    public String getCodigoContaEstrutural() {
+        return codigoContaEstrutural;
+    }
+
+    public void setCodigoContaEstrutural(String codigoContaEstrutural) {
+        this.codigoContaEstrutural = codigoContaEstrutural;
+    }
+
+    public String getComparacao() {
+        return comparacao;
+    }
+
+    public void setComparacao(String comparacao) {
+        this.comparacao = comparacao;
+    }
+
+    public void validate() {
+
+    }
+}
---
+++ parametrizacao-bi-impl/src/main/java/br/com/unicred/parametrizacao/bi/impl/business/domain/DefinicaoContasContabeisOrcadoRealizadoProjetos.java
@@ -0,0 +1,44 @@
+package br.com.unicred.parametrizacao.bi.impl.business.domain;
+
+public class DefinicaoContasContabeisOrcadoRealizadoProjetos {
+
+    private Integer codigoCooperativa;
+    private String codigoContaEstrutural;
+    private String comparacao;
+    private Boolean excluir;
+
+    protected DefinicaoContasContabeisOrcadoRealizadoProjetos() {
+
+    }
+
+    public static DefinicaoContasContabeisOrcadoRealizadoProjetos criarDefinicaoContasContabeisOrcadoRealizadoProjetos
+            (DefinicaoContasContabeisOrcadoRealizadoProjetosCommand command) {
+
+        command.validate();
+
+        return new DefinicaoContasContabeisOrcadoRealizadoProjetos(command);
+    }
+
+    public DefinicaoContasContabeisOrcadoRealizadoProjetos(DefinicaoContasContabeisOrcadoRealizadoProjetosCommand command) {
+        this.codigoCooperativa = command.getCodigoCooperativa();
+        this.codigoContaEstrutural = command.getCodigoContaEstrutural();
+        this.comparacao = command.getComparacao();
+        this.excluir = Boolean.FALSE;
+    }
+
+    public String getCodigoContaEstrutural() {
+        return codigoContaEstrutural;
+    }
+
+    public Integer getCodigoCooperativa() {
+        return codigoCooperativa;
+    }
+
+    public String getComparacao() {
+        return comparacao;
+    }
+
+    public Boolean getExcluir() {
+        return excluir;
+    }
+}
