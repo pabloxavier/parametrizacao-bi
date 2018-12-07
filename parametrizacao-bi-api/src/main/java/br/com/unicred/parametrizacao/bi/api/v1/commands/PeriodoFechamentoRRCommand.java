7606964421a02a47ed6d9ec85ae2b8aae3e4db78
caetano.vieira <caetano.vieira@UBR-RS-1899.e-unicred.com.br>
2018-12-07 15:23:53 -0200

    Criação do DAO e Commando do Período de Fechamento de Ranking Rating.

---
+++ parametrizacao-bi-api/src/main/java/br/com/unicred/parametrizacao/bi/api/v1/commands/PeriodoFechamentoRRCommand.java
@@ -0,0 +1,43 @@
+package br.com.unicred.parametrizacao.bi.api.v1.commands;
+
+import java.time.LocalDate;
+
+public class PeriodoFechamentoRRCommand {
+    
+    private LocalDate dataCompetencia;
+    private LocalDate dataUltimoProcessamento;
+    private boolean isFechado;
+    
+    public PeriodoFechamentoRRCommand(){
+        
+    }
+    
+   public void validate() {
+       //TODO realizar validacao
+   }
+
+    public LocalDate getDataCompetencia() {
+        return dataCompetencia;
+    }
+
+    public void setDataCompetencia(LocalDate dataCompetencia) {
+        this.dataCompetencia = dataCompetencia;
+    }
+
+    public LocalDate getDataUltimoProcessamento() {
+        return dataUltimoProcessamento;
+    }
+
+    public void setDataUltimoProcessamento(LocalDate dataUltimoProcessamento) {
+        this.dataUltimoProcessamento = dataUltimoProcessamento;
+    }
+
+    public boolean isFechado() {
+        return isFechado;
+    }
+
+    public void setFechado(boolean isFechado) {
+        this.isFechado = isFechado;
+    }
+
+}
