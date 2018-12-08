58457b85684088d66b90d1ae96a5f834d00487b5
mario.almeida <mario.almeida@unicred.com.br>
2018-12-07 22:59:05 -0200

    ajuste rest

--- parametrizacao-bi-impl/src/main/java/br/com/unicred/parametrizacao/bi/impl/api/v1/rest/DefinicaoContasContabeisOrcadoRealizadoProjetosRest.java
+++ parametrizacao-bi-impl/src/main/java/br/com/unicred/parametrizacao/bi/impl/api/v1/rest/DefinicaoContasContabeisOrcadoRealizadoProjetosRest.java
@@ -2,6 +2,7 @@ package br.com.unicred.parametrizacao.bi.impl.api.v1.rest;
 
 import br.com.unicred.parametrizacao.bi.api.v1.endpoints.DefaultEndpoint;
 import br.com.unicred.parametrizacao.bi.api.v1.representation.DefinicaoContasContabeisOrcadoRealizadoProjetosRepresentation;
+import br.com.unicred.parametrizacao.bi.impl.business.services.DefinicaoContasContabeisOrcadoRealizadoProjetosService;
 import io.swagger.annotations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,13 +11,13 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("ContasContabeisOrcadoRealizadoProjetos")
+@RequestMapping("/parametrizacao/bi/v1/ContasContabeisOrcadoRealizadoProjetos")
 public class DefinicaoContasContabeisOrcadoRealizadoProjetosRest extends DefaultEndpoint {
-   /* @Autowired
+    @Autowired
     private DefinicaoContasContabeisOrcadoRealizadoProjetosService service;
 
-    @RequestMapping(value = "/IncluirContas", method = RequestMethod.GET)
-    @ApiOperation(value = "Efetua a inclusão de novas Contas Contabeis.", response = String.class)
+
+    @ApiOperation(value = "Efetua a listagem de Contas Contabeis por Cooperativa.", response = String.class)
     @ApiImplicitParams({
             @ApiImplicitParam(name = "cooperativa", required = true, dataType = "integer", paramType = "header", value = "código da cooperativa") })
     @ApiResponses({ @ApiResponse(code = 200, message = "Requisição executada com sucesso."),
@@ -24,10 +25,10 @@ public class DefinicaoContasContabeisOrcadoRealizadoProjetosRest extends Default
             @ApiResponse(code = 422, message = "Erro de negócio ou validação."),
             @ApiResponse(code = 500, message = "Erro não esperado.") })
     @CrossOrigin(allowedHeaders = "*")
-    @GetMapping(value = "/ListarContasPorCooperativa")
-    public ResponseEntity<?> findByCooperativa(@PathVariable("codigoCooperativa") final Integer cdCoop){
-        List<DefinicaoContasContabeisOrcadoRealizadoProjetosRepresentation> conta = service.consultarPorCodigo(codigo);
-        return ok(ContaRepresentation.from(conta));
-    }*/
+    @RequestMapping(value = "/ListarContasPorCooperativa", method = RequestMethod.GET)
+    public ResponseEntity<?> findByCooperativa(@RequestHeader("Authorization") final String token) {
+       // List<DefinicaoContasContabeisOrcadoRealizadoProjetosRepresentation> conta = service.consultarPorCodigo(codigo);
+        return ok();
+    }
 
 }
